81c54ba29203116dc87d21f5d20cc259e213dfef
anseyyy <ahamedansil43@gmail.com>
2025-11-19 21:06:41 +0530

    Update: new features or fixes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,52 +2,68 @@ import React from 'react'
 
 function AboutUs() {
   return (
-    <div>
-        <div className="d-flex container  justify-content-center m-5">
-        <div className="" style={{ maxWidth: "50%" }}>
-          <div> <img src="/images/cildren.jpg" alt="" /></div>
-
-        </div>
-        <div className="ms-3" style={{ maxWidth: "50%" }}>
-          <h5 className="text-warning fw-bold">ABOUT US</h5>
-          <h1>Our Work Promise To Uphold The Trust Placed</h1>
-
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est corrupti atque eligendi fugiat, excepturi, molestias possimus dolorum hic architecto cupiditate. Dicta, nobis. Numquam non nulla iste ab dolor sunt.
-            Dolorem, fuga sunt! Nostrum ullam minima quae qui dolore ad assumenda cupiditate labore earum voluptates. In tempora, nesciunt quibusdam voluptatum maxime perferendis dolore natus possimus doloremque error voluptates quod fugiat.
-          </p>
-
-          <div className="m-2 d-flex">
-            <div style={{ maxWidth: "60%" }}>
-              <table className="table mt-3">
-                <tbody>
-                  <tr>
-                    <td><i class="fa-solid fa-check text-warning fw-bold"></i> Empower communities </td>
-                  </tr>
-                  <tr>
-                    <td><i class="fa-solid fa-check text-warning fw-bold"></i> Provide essential resources </td>
-                  </tr>
-                  <tr>
-                    <td><i class="fa-solid fa-check text-warning fw-bold"></i> Support global health</td>
-                  </tr>
-                  <tr>
-                    <td><i class="fa-solid fa-check text-warning fw-bold"></i> Promote sustainable development through donations</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div style={{ maxWidth: "40%" }} className="bg-warning rounded">
-              <div className="text-center text-light m-5">
-                <h1>28</h1> <h5>YEARS</h5> <p>OF EXPERIENCE</p>
-              </div>
-            </div>
-          </div>
-        </div>
 
 
-      </div>
+    <div className="my-5">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} lg={6} className="mb-4 mb-lg-0">
+            <img src="/images/cildren.jpg" alt="Children" className="img-fluid rounded" />
+          </Col>
 
 
+          <Col xs={12} lg={6}>
+            <h5 className="text-warning fw-bold">ABOUT US</h5>
+            <h1>Our Work Promise To Uphold The Trust Placed</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque est
+              corrupti atque eligendi fugiat, excepturi, molestias possimus dolorum
+              hic architecto cupiditate. Dicta, nobis. Numquam non nulla iste ab
+              dolor sunt. Dolorem, fuga sunt! Nostrum ullam minima quae qui dolore
+              ad assumenda cupiditate labore earum voluptates. In tempora, nesciunt
+              quibusdam voluptatum maxime perferendis dolore natus possimus
+              doloremque error voluptates quod fugiat.
+            </p>
+
+            <Row className="mt-4">
+              <Col xs={12} md={7}>
+                <ul className="list-unstyled">
+                  <li className="mb-2">
+                    <i className="fa-solid fa-check text-warning fw-bold me-2"></i>
+                    Empower communities
+                  </li>
+                  <li className="mb-2">
+                    <i className="fa-solid fa-check text-warning fw-bold me-2"></i>
+                    Provide essential resources
+                  </li>
+                  <li className="mb-2">
+                    <i className="fa-solid fa-check text-warning fw-bold me-2"></i>
+                    Support global health
+                  </li>
+                  <li className="mb-2">
+                    <i className="fa-solid fa-check text-warning fw-bold me-2"></i>
+                    Promote sustainable development through donations
+                  </li>
+                </ul>
+              </Col>
+
+
+              <Col xs={12} md={5}>
+                <div className="bg-warning rounded text-center text-light p-4 mt-3 mt-md-0">
+                  <h1>28</h1>
+                  <h5>YEARS</h5>
+                  <p>OF EXPERIENCE</p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
+
+
+
+
   )
 }
 
